bc76a92617fd98200d3a66398fe884242ef56e39
madhusmita953 <mca.madhu87@gmail.com>
2026-06-26 15:45:22 +0530

    Add JPA Entity annotations to TouristPlace model

--- src/main/java/com/smarttourism/model/TouristPlace.java
+++ src/main/java/com/smarttourism/model/TouristPlace.java
@@ -1,14 +1,20 @@
 package com.smarttourism.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "tourist_place")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TouristPlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String city;
     private String placeName;
     private String description;
